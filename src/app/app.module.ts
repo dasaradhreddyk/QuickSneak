a34b2f7009b873833640b3dd97b300ec8512ab36
Dasaradha Kalapureddy <dasaradha.kalapureddy@tal.com.au>
2022-07-29 16:32:02 +1000

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+   
 import { AppComponent } from './app.component';
+   
+import { DataTablesModule } from 'angular-datatables';
+import { HttpClientModule } from '@angular/common/http';
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
+import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
+import { VideolistComponent } from './videolist/videolist.component';
 
+
+   
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideolistComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DataTablesModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
+
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
